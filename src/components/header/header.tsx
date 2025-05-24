cbfb3612328b2fa4cca99b70320ac8faabbf7b64
dagsevenonur <onurdagseven01@gmail.com>
2025-05-24 16:03:12 +0300

    feat: header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <header className="container">
+      <h1 className="title">Typing Speed Tester</h1>
+      <h2 className="description">
+        Press the <strong>start</strong> button and start typing!
+      </h2>
+    </header>
+  );
+}
